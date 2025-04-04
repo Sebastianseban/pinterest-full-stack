4c7fb08ddb01c903765a1a40ae73f9f73b935bc6
sebastian <sebastianseban0105@gmail.com>
2025-04-04 12:05:31 +0530

    implement comments section with layout and styling

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "../image/Image";
+import "./comments.css";
 
 const Comments = () => {
   return (
-    <div>Comments</div>
-  )
-}
+    <div className="comments">
+      <div className="commentList">
+        <span className="commentsCount">5 comments</span>
+        {/* COMMENTS */}
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentConten">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+            <span className="commentTime">1hr</span>
+          </div>
+        </div>
+      </div>
+      <form className="commentForm">
+        <input type="text" placeholder="add a comment" />
+        <div className="emoji">
+          <div>😍</div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Comments
+export default Comments;
